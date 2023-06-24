c69e8a7d2f1c752e4fab45af90c5b43b7a463e8b
naldal <songhamin@songhamin-ui-MacBookAir.local>
2023-06-25 00:06:10 +0900

    🚧 fix moya issue: the original moya from github has minimum version is 10
    Fork it and upgrade the version to 11.0

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -7,7 +7,7 @@ let dependencies = Dependencies(
     .remote(url: "https://github.com/Quick/Nimble.git", requirement: .branch("main")),
     .remote(url: "https://github.com/SnapKit/SnapKit.git", requirement: .exact("5.0.1")),
     .remote(url: "https://github.com/ReactiveX/RxSwift", requirement: .exact("6.5.0")),
-    .remote(url: "https://github.com/Moya/Moya", requirement: .exact("15.0.3")),
+    .remote(url: "https://github.com/naldal/Moya", requirement: .branch("master")),
     .remote(url: "https://github.com/devxoul/Then", requirement: .exact("3.0.0"))
   ], productTypes: [
     "Quick": .framework,
