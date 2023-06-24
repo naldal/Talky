18955a64166f2b6de891f2cda28cdc33d8011205
naldal <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>
2023-06-24 21:04:16 +0900

    ➕ add fonts 🚧 add UIFont Extension to use easier and reusable

---
+++ Projects/Talky/Targets/Talky/Sources/Extensions/UIFont+Extensions.swift
@@ -0,0 +1,50 @@
+//
+//  UIFont+Extensions.swift
+//  Talky
+//
+//  Created by 송하민 on 2023/06/24.
+//  Copyright © 2023 organizationName. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIFont {
+  
+  static var jetbrain13M: UIFont {
+    return JetBrainsFont.medium.font(size: 13)
+  }
+  
+  static var jetbrain14: UIFont {
+    return JetBrainsFont.medium.font(size: 14)
+  }
+  
+  static var jetbrain15: UIFont {
+    return JetBrainsFont.medium.font(size: 15)
+  }
+  
+  static var jetbrain13R: UIFont {
+    return JetBrainsFont.regular.font(size: 13)
+  }
+  
+  static var jetbrain14R: UIFont {
+    return JetBrainsFont.regular.font(size: 14)
+  }
+  
+  static var jetbrain15R: UIFont {
+    return JetBrainsFont.regular.font(size: 15)
+  }
+  
+  static var jetbrain13B: UIFont {
+    return JetBrainsFont.bold.font(size: 13)
+  }
+  
+  static var jetbrain14B: UIFont {
+    return JetBrainsFont.bold.font(size: 14)
+  }
+  
+  static var jetbrain15B: UIFont {
+    return JetBrainsFont.bold.font(size: 15)
+  }
+  
+}
