38505794d44c59c26b06594103a76958f0fb7cc4
naldal <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>
2023-06-24 21:05:06 +0900

    🚧 add Assets alias (vanilla TuistBundle enum name is too long to use)

---
+++ Projects/Talky/Targets/Talky/Resources/BundleAlias/Alias.swift
@@ -0,0 +1,13 @@
+//
+//  Alias.swift
+//  Talky
+//
+//  Created by 송하민 on 2023/06/24.
+//  Copyright © 2023 organizationName. All rights reserved.
+//
+
+import Foundation
+
+typealias JetBrainsFont = TalkyFontFamily.JetBrainsMono
+typealias Color = TalkyAsset.Colors
+typealias Image = TalkyAsset.Images
